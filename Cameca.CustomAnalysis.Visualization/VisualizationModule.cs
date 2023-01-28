3958bd3f8f8a48e9a0530bf87b227cfec31deff6
Jim Payne <jim.payne@ametek.com>
2023-01-27 23:20:54 -0600

    Add project files.

---
+++ Cameca.CustomAnalysis.Visualization/VisualizationModule.cs
@@ -0,0 +1,20 @@
+﻿using Cameca.CustomAnalysis.Utilities;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Cameca.CustomAnalysis.Visualization;
+
+/// <summary>
+/// Public <see cref="IModule"/> implementation is the entry point for AP Suite to discover and configure the custom analysis
+/// </summary>
+public class VisualizationModule : IModule
+{
+	public void RegisterTypes(IContainerRegistry containerRegistry)
+	{
+		containerRegistry.AddCustomAnalysisUtilities(options => options.UseStandardBaseClasses = true);
+	}
+
+	public void OnInitialized(IContainerProvider containerProvider)
+	{
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Visualization
+## CAMECA Instruments Inc.
+## Copyright 2023 © CAMECA Instruments Inc. All rights reserved.
